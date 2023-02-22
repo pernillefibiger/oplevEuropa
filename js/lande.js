28f36e356b9af62e2a6fb3b8bb99bf410eb62c25
Katjahkrogh <you@example.com>
2023-02-22 11:32:42 +0100

    template

---
+++ js/lande.js
@@ -0,0 +1,15 @@
+const url = "https://opleveuropa-c5da.restdb.io/rest/_swagger.json";
+
+const options = {
+  headers: {
+    "x-apikey": "	63ef7557478852088da683e0",
+  },
+};
+
+async function hentData() {
+  const repons = await fetch(url, options);
+  const json = await repons.json();
+  vis(json);
+}
+
+
