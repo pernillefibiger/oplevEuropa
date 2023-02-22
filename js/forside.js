94b6ec5b180c1b868e8a5b8dbf6b07280b4b3866
Victor Bentzen <vict656m@stud.kea.dkgit config --global user.email vict656m@stud.kea.dk>
2023-02-22 14:03:23 +0100

    update

--- js/forside.js
+++ js/forside.js
@@ -13,3 +13,51 @@ function myFunction() {
     meretext.style.display = "inline";
   }
 }
+
+function myFunction2() {
+  var dots = document.getElementById("dots2");
+  var meretext = document.getElementById("mere2");
+  var tekstknap = document.getElementById("læsmereknap2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    tekstknap.innerHTML = "Læs mere";
+    meretext.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    tekstknap.innerHTML = "Læs mindre";
+    meretext.style.display = "inline";
+  }
+}
+
+function myFunction3() {
+  var dots = document.getElementById("dots3");
+  var meretext = document.getElementById("mere3");
+  var tekstknap = document.getElementById("læsmereknap3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    tekstknap.innerHTML = "Læs mere";
+    meretext.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    tekstknap.innerHTML = "Læs mindre";
+    meretext.style.display = "inline";
+  }
+}
+
+function myFunction4() {
+  var dots = document.getElementById("dots4");
+  var meretext = document.getElementById("mere4");
+  var tekstknap = document.getElementById("læsmereknap4");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    tekstknap.innerHTML = "Læs mere";
+    meretext.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    tekstknap.innerHTML = "Læs mindre";
+    meretext.style.display = "inline";
+  }
+}
